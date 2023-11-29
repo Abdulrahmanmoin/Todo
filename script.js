fd5d1f19ca312103e0691883c04bdbfcfa706d82
Abdulrahmanmoin <149314900+Abdulrahmanmoin@users.noreply.github.com>
2023-11-29 23:43:49 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+let btn = document.querySelector("button");
+let ul = document.querySelector("ul");
+let input = document.querySelector("input");
+
+btn.addEventListener("click" , function () {
+    let item = document.createElement("li");
+    item.innerText = input.value;
+
+    let delBtn = document.createElement("button");
+    delBtn.innerText = "Delete";
+    delBtn.classList.add("delete");
+
+    item.appendChild(delBtn);
+    ul.appendChild(item);
+    input.value = "";
+});
+
+ul.addEventListener("click", function (event) {
+    if (event.target.nodeName === "BUTTON") {
+        let  listItems = event.target.parentElement;
+        listItems.remove();
+        console.log("Deleted");
+    } 
+});
+
